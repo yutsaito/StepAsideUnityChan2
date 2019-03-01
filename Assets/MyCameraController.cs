0cd74536dde26ec7cf53ac2864c208fadb52df51
yutakasaito <fvkibdcz7@yahoo.co.jp>
2019-03-01 19:35:16 +0900

    20190301　　 lesson6 　　　4.10 ユニティちゃんがジャンプするスクリプトを作成でエラーがでる
    メンターさんこんばんは。
    どうもうまくいきません。
    以下のエラーがでます。
    スペースキーを押すと、ｼﾞｬﾝﾌﾟのはずが
    Platform does not support compute shaders
    他に警告がでます
    d3d11: failed to create buffer (target 0x10 mode 0 size 79200) since compute isn't supported at this feature level
    どうすればよろしいでしょうか？
    よろしくお願いします。

---
+++ Assets/MyCameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCameraController : MonoBehaviour
+{
+    //Unityちゃんのｵﾌﾞｼﾞｪｸﾄ
+    private GameObject unitychan;
+    //Unityちゃんとカメラの距離
+    private float difference;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Unityちゃんのオブジェクトを取得
+        //unitychan = GameObject.Find("UnityChan"); // thisがないとダメ？
+        this.unitychan = GameObject.Find("unitychan");
+
+        //Unityちゃんとカメラの位置(z座標)の差を求める
+        difference = unitychan.transform.position.z - this.transform.position.z;   
+            //ピタゴラスで計算するのだろうと考え、メンドくさ、と思ってしまった。Z座標だけでよかった
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Unityちゃんの位置に合わせてカメラの位置を移動
+        //this.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + difference);
+        //惜しかった
+        this.transform.position = new Vector3(0, this.transform.position.y, this.unitychan.transform.position.z - difference);
+    }
+}
---
+++ Assets/UnityChanController.cs
@@ -0,0 +1,135 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class UnityChanController : MonoBehaviour
+//{
+//    //アニメーションするためのコンポーネントを入れる
+//    private Animator myAnimator;
+
+//    //Unityちゃんを移動させるｺﾝﾎﾟｰﾈﾝﾄを入れる
+//    Rigidbody myRigidbody;
+
+//    //前進するための力（の大きさ）
+//    private float forwardForce = 800.0f;
+
+//    //左右に移動するための力
+//    private float turnForce = 500.0f;
+//    //ｼﾞｬﾝﾌﾟするための力
+//    private float upForce = 500.0f;
+//    //左右の移動できる範囲
+//    private float movableRange = 3.4f;
+
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        //Animatorｺﾝﾎﾟｰﾈﾝﾄを取得
+//        //  myAnimator = this.GetComponent<Animator>();   //文法的にはエラーでないみたい、使えないかな？
+//        this.myAnimator = GetComponent<Animator>();     //こう書けるのはこのｽｸﾘﾌﾟﾄがUnityChan自体にｱﾀｯﾁされているから
+//        //走るアニメーションを開始
+//        //myAnimator = SetActive(true);  感で書いたが外れた myAnimatorの後の"."が出ないのが情けない・・・
+//        myAnimator.SetFloat("Speed", 1);
+//        //RigidBodyコンポーネントを取得
+//        myRigidbody = GetComponent<Rigidbody>();
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        //UnityChanに前方向の力を加える
+//        // GetComponent<RigidBody>().AddFore  //見ずにはかけず
+//        //this.myRigidbody.AddForce=Vector3()
+//        this.myRigidbody.AddForce(this.transform.forward * this.forwardForce);      //全然かけなかった・・・
+//        //Unityちゃんを矢印キーまたはﾎﾞﾀﾝに応じて左右に移動させる
+//        //if (Input.GetKeyDown(KeyCode = LeftArrow)) {this.myRigidbody.Addforece(this.transform.)}
+//        //if(Input.GetKey(KeyCode="LeftArrow")||)
+//        //かけなかったので写す
+//        if(Input.GetKey(KeyCode.LeftArrow)&& -this.movableRange < this.transform.position.x) {
+//            //左に移動
+//            this.myRigidbody.AddForce(-this.turnForce, 0,0);
+//        }else if (Input.GetKey(KeyCode.RightArrow) && this.movableRange > this.myRigidbody.transform.position.x)
+//        {
+//            //右に移動
+//            this.myRigidbody.AddForce(this.turnForce, 0, 0);
+//        }
+//        //ジャンプしていない時にｽﾍﾟｰｽが押されたらｼﾞｬﾝﾌﾟする
+//        if (Input.GetKeyDown(KeyCode.Space) && this.transform.position.y < 0.5f)
+//        {
+//            this.myAnimator.SetBool("Jump",true);
+//            //Unityちゃんに上方向の力を加える
+//            this.myRigidbody.AddForce(this.transform.up*this.upForce); 
+//        }
+//    }
+//}
+
+//3Dで移動させるときは、RigidBodyで！
+
+
+
+
+using UnityEngine;
+using System.Collections;
+
+public class UnityChanController : MonoBehaviour
+{
+    //アニメーションするためのコンポーネントを入れる
+    private Animator myAnimator;
+    //Unityちゃんを移動させるコンポーネントを入れる
+    private Rigidbody myRigidbody;
+    //前進するための力
+    private float forwardForce = 800.0f;
+    //左右に移動するための力
+    private float turnForce = 500.0f;
+    //ジャンプするための力（追加）
+    private float upForce = 500.0f;
+    //左右の移動できる範囲
+    private float movableRange = 3.4f;
+
+    void Start()
+    {
+        //アニメータコンポーネントを取得
+        this.myAnimator = GetComponent<Animator>();
+
+        //走るアニメーションを開始
+        this.myAnimator.SetFloat("Speed", 1);
+
+        //Rigidbodyコンポーネントを取得（追加）
+        this.myRigidbody = GetComponent<Rigidbody>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Unityちゃんに前方向の力を加える
+        this.myRigidbody.AddForce(this.transform.forward * this.forwardForce);
+
+        //Unityちゃんを矢印キーまたはボタンに応じて左右に移動させる
+        if ((Input.GetKey(KeyCode.LeftArrow)) && -this.movableRange < this.transform.position.x)
+        {
+            //左に移動
+            this.myRigidbody.AddForce(-this.turnForce, 0, 0);
+        }
+        else if ((Input.GetKey(KeyCode.RightArrow)) && this.transform.position.x < this.movableRange)
+        {
+            //右に移動
+            this.myRigidbody.AddForce(this.turnForce, 0, 0);
+        }
+
+        //Jumpステートの場合はJumpにfalseをセットする（追加）
+        if (this.myAnimator.GetCurrentAnimatorStateInfo(0).IsName("Jump"))
+        {
+            this.myAnimator.SetBool("Jump", false);
+        }
+
+        //ジャンプしていない時にスペースが押されたらジャンプする（追加）
+        if (Input.GetKeyDown(KeyCode.Space) && this.transform.position.y < 0.5f)
+        {
+            //ジャンプアニメを再生（追加）
+            this.myAnimator.SetBool("Jump", true);
+            //Unityちゃんに上方向の力を加える（追加）
+            this.myRigidbody.AddForce(this.transform.up * this.upForce);
+        }
+    }
+}
